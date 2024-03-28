a1710719f9b2947c50842f2829b2fbcc17fa08d8
David Rauf <davidrauf@gmail.com>
2024-03-28 19:27:41 -0400

    version 2.2 dark mode working

---
+++ assets/js/logic.js
@@ -0,0 +1,23 @@
+let darkMode = "no";
+const darkModeToggle = document.querySelector('#mode');
+
+darkModeToggle.addEventListener("click", () => {
+    if (darkMode !== "yes"){
+        enableDarkMode();
+        darkMode = "yes";
+        console.log(darkMode)
+    }else{
+        disableDarkMode();
+        darkMode = "no";
+        console.log(darkMode);
+    };
+});
+
+const enableDarkMode = () => {
+    document.body.classList.add("darkmode");
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+}
+
